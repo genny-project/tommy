69eda3e4ad76580bf7cd2ae7fabfadbb7dfc0ba4
Adam Crow <adamcrow63@gmail.com>
2020-08-17 16:01:23 +1000

    Updated

---
+++ deploy-all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+BUILD_NUMBER=`git rev-list --all|wc -l|xargs`
+
+#printf "What would you like to build?\n"
+#select platform in "Android App Bundle" "Android APK" "iOS"; do
+#  echo "Preparing build #$BUILD_NUMBER"
+#  case $platform in
+#      "Android App Bundle" ) flutter build appbundle --build-number $BUILD_NUMBER; break;;
+#      "Android APK" ) flutter build apk --build-number $BUILD_NUMBER; break;;
+#      "iOS" ) flutter build ios --build-number $BUILD_NUMBER; break;;
+#  esac
+#done
+
+flutter clean;
+
+flutter build ios --build-number $BUILD_NUMBER;
+cd ios
+fastlane internal
+cd ..
+
