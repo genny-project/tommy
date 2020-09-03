edc006727b04bf13a9fd48f495770c2bbc33701f
Barad Ghimire <baradghimire@gmail.com>
2020-09-03 16:42:06 +1000

    WIP

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import './startapp/StartApp.dart';
 import './ProjectEnv.dart';
+import 'push_nofitications.dart.dart';
 
 void main() async {
-  
+    PushNotificationsManager().init();
     runApp(new MaterialApp(
       debugShowCheckedModeBanner: false,
       title: ProjectEnv.projectName,
