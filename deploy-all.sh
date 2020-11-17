2e4aba01efa737ec0f44c532ec45b5c42dcbef3f
Barad Ghimire <baradghimire@gmail.com>
2020-11-18 10:57:18 +1100

    Bump to 7.6.0, and test deploy 7.6.0

--- deploy-all.sh
+++ deploy-all.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 BUILD_NUMBER=`git rev-list --all|wc -l|xargs`
+BUILD_NAME=`git branch --show-current`
+#BUILD_NUMBER="82"
+#BUILD_NAME="7.6.0"
 
 #printf "What would you like to build?\n"
 #select platform in "Android App Bundle" "Android APK" "iOS"; do
@@ -10,11 +13,10 @@ BUILD_NUMBER=`git rev-list --all|wc -l|xargs`
 #      "iOS" ) flutter build ios --build-number $BUILD_NUMBER; break;;
 #  esac
 #done
-
+./swap-endpoint.sh internmatch-dev.gada.io
 flutter clean;
 #flutter build appbundle --build-number $BUILD_NUMBER
-flutter build ios --build-number $BUILD_NUMBER;
+flutter build ios --build-name $BUILD_NAME --build-number $BUILD_NUMBER
 cd ios
 fastlane internal --verbose
 cd ..
-
