7ce07c8ee3a2fe0e12929996b33941bc4966d2ee
Barad Ghimire <baradghimire@gmail.com>
2020-11-24 15:52:55 +1100

    update ios beta workflow

--- deploy-all.sh
+++ deploy-all.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 BUILD_NUMBER=`git rev-list --all|wc -l|xargs`
 BUILD_NAME=`git branch --show-current`
-#BUILD_NUMBER="82"
-#BUILD_NAME="7.6.0"
 
 #printf "What would you like to build?\n"
 #select platform in "Android App Bundle" "Android APK" "iOS"; do
