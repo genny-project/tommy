56f1af92b5411532d771beb51847be54bb619d70
Barad Ghimire <baradghimire@gmail.com>
2020-11-26 16:12:46 +1100

    Run again with new gh Secrets

--- deploy-all.sh
+++ deploy-all.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 BUILD_NUMBER=`git rev-list --all|wc -l|xargs`
 BUILD_NAME=`git branch --show-current`
-
-#printf "What would you like to build?\n"
-#select platform in "Android App Bundle" "Android APK" "iOS"; do
-#  echo "Preparing build #$BUILD_NUMBER"
-#  case $platform in
-#      "Android App Bundle" ) flutter build appbundle --build-number $BUILD_NUMBER; break;;
-#      "Android APK" ) flutter build apk --build-number $BUILD_NUMBER; break;;
-#      "iOS" ) flutter build ios --build-number $BUILD_NUMBER; break;;
-#  esac
-#done
 ./swap-endpoint.sh internmatch-mobile.gada.io
 flutter clean
-#flutter build appbundle --build-number $BUILD_NUMBER
 flutter build ios --build-name $BUILD_NAME --build-number $BUILD_NUMBER
 cd ios
 fastlane internal
