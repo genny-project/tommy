6f13f0fe5fac2a4d95160e4641bbb07fd55ff088
Barad Ghimire <baradghimire@gmail.com>
2020-11-27 15:58:45 +1100

    hard-code code-signing

--- deploy-all.sh
+++ deploy-all.sh
@@ -3,7 +3,7 @@ BUILD_NUMBER=`git rev-list --all|wc -l|xargs`
 BUILD_NAME=`git branch --show-current`
 ./swap-endpoint.sh internmatch-mobile.gada.io
 flutter clean
-flutter build ios --build-name $BUILD_NAME --build-number $BUILD_NUMBER
+flutter build ios --build-name $BUILD_NAME --build-number $BUILD_NUMBER --no-codesign
 cd ios
 fastlane internal
 cd ..
