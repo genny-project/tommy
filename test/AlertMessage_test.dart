2624942cd10d21031f9c087d064e495fae3a3993
Thomas Crow <thomas.crow@crowtech.com.au>
2020-12-14 12:01:59 +1100

    alert message test still needs looking into

--- test/AlertMessage_test.dart
+++ test/AlertMessage_test.dart
@@ -1,37 +1,42 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import '../lib/widgets/AlertMessage.dart';
-import 'package:mockito/mockito.dart';
+//import 'package:mockito/mockito.dart';
 
-class MockBuildContext extends Mock implements BuildContext {}
+//class MockBuildContext extends Mock implements BuildContext {}
+
+class TestWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: showAlertMessage('find this', context),
+      ),
+    );
+  }
+}
 
 void main() {
-  MockBuildContext _mockContext = MockBuildContext();
+  //MockBuildContext _mockContext = MockBuildContext();
   testWidgets('showAlertMessage widget test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MaterialApp(
-      home: Scaffold(
-        body: showAlertMessage('find this', _mockContext),
-      ),
-    ));
+    //Build our app and trigger a frame.
+    await tester.pumpWidget(TestWidget());
 
-    // Verify that our counter starts at 0.
-    expect(find.text("Information"), findsOneWidget);
-    //expect(find.text('1'), findsNothing);
+    // await tester.pumpWidget(MaterialApp(
+    //   title: 'Flutter Demo',
+    //   home: Scaffold(
+    //     appBar: AppBar(
+    //       title: Text('title'),
+    //     ),
+    //     body: Center(
+    //       child: Text('find this'),
+    //     ),
+    //   ),
+    // ));
 
-    // Tap the '+' icon and trigger a frame.
-    //await tester.tap(find.byIcon(Icons.add));
-    //await tester.pump();
+    tester.pumpAndSettle();
 
-    // Verify that our counter has incremented.
-    //expect(find.text('0'), findsNothing);
-    //expect(find.text('1'), findsOneWidget);
+    //Future(() => expect(find.text('find this'), findsOneWidget));
+    expect(find.text('find this'), findsOneWidget);
   });
 }
