82664d2cc4b52221fa7fc62fdb7f732e43b4983b
Adam Crow <adamcrow63@gmail.com>
2021-09-02 07:46:25 +1000

    Updated to 8.0.2

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -2,17 +2,19 @@
 // Generated file. Do not edit.
 //
 
-// ignore: unused_import
-import 'dart:ui';
+// ignore_for_file: directives_ordering
+// ignore_for_file: lines_longer_than_80_chars
 
+import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
-void registerPlugins(PluginRegistry registry) {
-  SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
-  UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
-  registry.registerMessageHandler();
+void registerPlugins(Registrar registrar) {
+  FirebaseCoreWeb.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
 }
