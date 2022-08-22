c062570b32fd596fa1183f5f857f16e3003d0542
Xiang <chen@localhost>
2022-08-22 14:02:27 +1000

    update grpc url and port for remote connnection

--- lib/projectenv.dart
+++ lib/projectenv.dart
@@ -1,14 +1,12 @@
-
-
 // ignore_for_file: prefer_interpolation_to_compose_strings
 
 class ProjectEnv {
   static const String baseUrl = "https://lojing-dev.gada.io";
   // static const String baseUrl = "http://10.0.2.2";
-  static const String grpcUrl = "10.0.2.2";
+  // static const String grpcUrl = "10.0.2.2";
+  static const String grpcUrl = "lojing-dev.gada.io";
   static const String apiVersion = "/v7";
   static const String projectName = "InternMatch";
   static const String httpURL = baseUrl + apiVersion + "/api/service/sync";
   static const String devicesUrl = baseUrl + apiVersion + "/api/devices";
-
 }
--- lib/utils/proto_utils.dart
+++ lib/utils/proto_utils.dart
@@ -1,12 +1,12 @@
 import 'package:grpc/grpc.dart';
 import 'package:tommy/projectenv.dart';
 
-
 class ProtoUtils {
   static ClientChannel getChannel() {
     ClientChannel channel = ClientChannel(
       ProjectEnv.grpcUrl,
-      port: 5154,
+      // port: 5154,
+      port: 30299,
       options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
     );
     return channel;
